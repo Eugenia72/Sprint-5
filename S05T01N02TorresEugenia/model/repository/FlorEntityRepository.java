7e8e889d369fad4325d94895dc7dc28083c6ddc6
Eugenia72 <162119849+Eugenia72@users.noreply.github.com>
2024-07-09 18:02:26 +0200

    Add files via upload

---
+++ S05T01N02TorresEugenia/model/repository/FlorEntityRepository.java
@@ -0,0 +1,7 @@
+package cat.itacademy.barcelonactiva.torres.eugenia.s05.t01.n02.S05T01N02TorresEugenia.model.repository;
+
+import cat.itacademy.barcelonactiva.torres.eugenia.s05.t01.n02.S05T01N02TorresEugenia.model.domain.FlorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlorEntityRepository extends JpaRepository<FlorEntity, Integer> {
+}
