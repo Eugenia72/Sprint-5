6e09cf6ad881c618b23c9b762a40ef8d10c2b935
Eugenia72 <162119849+Eugenia72@users.noreply.github.com>
2024-07-13 12:39:45 +0200

    Delete S05T01N02TorresEugenia/S05T01N02TorresEugenia directory

--- S05T01N02TorresEugenia/S05T01N02TorresEugenia/src/main/java/cat/itacademy/barcelonactiva/torres/eugenia/s05/t01/n02/S05T01N02TorresEugenia/excepcion/FlorNoEncontradaException.java
+++
@@ -1,8 +0,0 @@
-package cat.itacademy.barcelonactiva.torres.eugenia.s05.t01.n02.S05T01N02TorresEugenia.excepcion;
-
-public class FlorNoEncontradaException extends RuntimeException {
-    public FlorNoEncontradaException(String message) {
-        super(message);
-    }
-}
-
